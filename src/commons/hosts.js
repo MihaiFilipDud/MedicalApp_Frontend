b72a1f7724a2e27bd6a08497ad219cb71954979e
MihaiFilipDud <mihaifilipdud@gmail.com>
2021-01-14 05:08:55 +0200

    try

--- src/commons/hosts.js
+++ src/commons/hosts.js
@@ -1,4 +1,4 @@
 export const HOST = {
     //backend_api: 'http://localhost:8080',
-    backend_api: 'https://mihaifilipdud-frontend-sd.herokuapp.com/'
+    backend_api: 'https://mihaifilipdud-frontend-sd.herokuapp.com'
 };
